59a78981eb0eab5eb4c03df94ca5574ee51082c5
taoalu <taoalu@gmail.com>
2017-01-04 12:27:09 -0300

    ...

--- HTML5Application/public_html/js/meujs.js
+++ HTML5Application/public_html/js/meujs.js
@@ -3,14 +3,32 @@ function Login() {
     var usuario = document.getElementById("loginUsuario").value;
     usuario = usuario.toLowerCase();
     var senha = document.getElementById("loginSenha").valeu;
+    senha = senha.toString();
 
-    if (usuario === "admin" && senha === "admin") {
+    if (usuario == "admin" && senha == "admin") {
         alert("Sucesso");
         alert("Usuário: " + usuario + "  " + "Senha: " + senha);
         done = 1;
     }
     if (done === 0) {
         alert("Dados incorretos, tente novamente");
+    }
+}
+
+function Registro() {    
+    var done = 0;
+    var usuario = document.getElementById("registroNome").value;
+    usuario = usuario.toLowerCase();
+    var senha = document.getElementById("registroSenha").valeu;
+    
+
+    if (usuario !== "" && senha !== "") {
+        alert("Sucesso");
+        alert("Usuário: " + usuario + "  " + "Senha: " + senha);
+        done = 1;
+    }
+    if (done === 0) {
+        alert("Preencha todos os campos e tente novamente");
     }
 }
 
